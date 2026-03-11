3c5233c33c16f7d78e835e62dff47ee4534ef802
Zane Aladi <zanealadi@gmail.com>
2026-03-10 22:52:22 -0500

    initializing project, used maven for springboot scaffold

---
+++ src/main/java/com/zane/nba_tracker/NbaTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.zane.nba_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zane/nba_tracker/NbaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zane.nba_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
